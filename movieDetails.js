741d1c628b4c4cb294037efac9d84dbab9d4206a
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-10-11 20:13:14 +0530

    Update movieDetails.js

--- movieDetails.js
+++ movieDetails.js
@@ -357,7 +357,7 @@ const html2 = function (moviee) {
         </div>
         <div class="playButtonContainer"> 
        
-        <a class="playLink" href="https://www.2embed.to/embed/imdb/movie?id=${moviee.imdb_id}"><button class="play_btn"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19"
+        <a class="playLink" href="https://embed.smashystream.com/playere.php?imdb=IMDB_ID"><button class="play_btn"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19"
                 fill="currentColor" class="path_btn bi-play-fill" viewBox="0 0 16 16">
                 <path class="path_btnn"
                     d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z">
